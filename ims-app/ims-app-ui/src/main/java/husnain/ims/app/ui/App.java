0d5b5ce9019d7eb338e8769d1b6f07e716265210
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-24 18:34:05 +0300

    Create App.java

---
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/App.java
@@ -0,0 +1,48 @@
+package husnain.ims.app.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Husnain Arif
+ */
+public class App extends Application {
+
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
+
+    @Override
+    public void start(Stage stage) {
+        Scene scene = null;
+        
+        try {
+            scene = new Scene(this.createParent());
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+
+        stage.setTitle("Inventory Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private Parent createParent() throws IOException {
+        Parent root = null;
+
+        URL url = this.getClass().getResource("MainForm.fxml");
+
+        return FXMLLoader.load(url);
+    }
+
+}
