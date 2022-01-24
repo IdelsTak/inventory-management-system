8953231cece8051a7f6ba37e71c86a1ab595946d
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-24 18:20:10 +0300

    Create MainFormController.java

---
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/MainFormController.java
@@ -0,0 +1,20 @@
+package husnain.ims.app.ui.controllers;
+
+import javafx.fxml.FXML;
+
+/**
+ * FXML Controller class
+ *
+ * @author Husnain Arif
+ */
+public class MainFormController {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    public void initialize() {
+        // TODO
+    }
+
+}
