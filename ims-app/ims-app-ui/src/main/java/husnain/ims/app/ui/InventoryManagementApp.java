9613be04402185ec9f6016cf08ddcd50b0c415dd
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-24 20:45:45 +0300

    Create InventoryManagementApp.java

---
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/InventoryManagementApp.java
@@ -0,0 +1,44 @@
+package husnain.ims.app.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Husnain Arif
+ */
+public class InventoryManagementApp extends Application {
+
+    private static final Logger LOG = Logger.getLogger(InventoryManagementApp.class.getName());
+
+    @Override
+    public void start(Stage stage) {
+        Scene scene = null;
+
+        try {
+            scene = new Scene(this.createParent());
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+
+        stage.setTitle("Inventory Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private Parent createParent() throws IOException {
+        return FXMLLoader.load(this.getClass().getResource("MainForm.fxml"));
+    }
+
+}
