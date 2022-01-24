c87263b6d16f8f001f5b3cab426261596d728e09
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-24 19:36:39 +0300

    Update MainFormController.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/MainFormController.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/MainFormController.java
@@ -1,6 +1,13 @@
 package husnain.ims.app.ui.controllers;
 
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+import javafx.beans.binding.DoubleExpression;
+import javafx.beans.property.Property;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Pair;
 
 /**
  * FXML Controller class
@@ -9,12 +16,76 @@ import javafx.fxml.FXML;
  */
 public class MainFormController {
 
+    private static final Logger LOG = Logger.getLogger(MainFormController.class.getName());
+    @FXML
+    private TableColumn<?, ?> assocInvLevelColumn;
+
+    @FXML
+    private TableColumn<?, ?> assocPartIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> assocPartNameColumn;
+
+    @FXML
+    private TableView<?> assocPartsTable;
+
+    @FXML
+    private TableColumn<?, ?> assocPriceColumn;
+
+    @FXML
+    private TableColumn<?, ?> invLevelColumn;
+
+    @FXML
+    private TableColumn<?, ?> partIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> partNameColumn;
+
+    @FXML
+    private TableView<?> partsTable;
+
+    @FXML
+    private TableColumn<?, ?> priceColumn;
+
     /**
      * Initializes the controller class.
      */
     @FXML
     public void initialize() {
-        // TODO
+        partsTable.widthProperty().addListener((ov, t, t1) -> {
+            this.initColumnBinding(
+                    partsTable.widthProperty(),
+                    Stream.of(
+                            new Pair(partIdColumn.maxWidthProperty(), 0.15),
+                            new Pair(partNameColumn.maxWidthProperty(), 0.35),
+                            new Pair(invLevelColumn.maxWidthProperty(), 0.25),
+                            new Pair(priceColumn.maxWidthProperty(), 0.25)
+                    )
+            );
+        });
+        assocPartsTable.widthProperty().addListener((ov, t, t1) -> {
+            this.initColumnBinding(
+                    assocPartsTable.widthProperty(),
+                    Stream.of(
+                            new Pair(assocPartIdColumn.maxWidthProperty(), 0.15),
+                            new Pair(assocPartNameColumn.maxWidthProperty(), 0.35),
+                            new Pair(assocInvLevelColumn.maxWidthProperty(), 0.25),
+                            new Pair(assocPriceColumn.maxWidthProperty(), 0.25)
+                    )
+            );
+        });
+    }
+
+    private void initColumnBinding(DoubleExpression widthProp, Stream<Pair<Property, Double>> pairs) {
+        pairs.forEach(p -> this.bindColumnWidth(widthProp, p.getKey(), p.getValue()));
+    }
+
+    private void bindColumnWidth(DoubleExpression tableProp, Property colProp, double percent) {
+        if (colProp.isBound()) {
+            colProp.unbind();
+        }
+
+        colProp.bind(tableProp.multiply(percent));
     }
 
 }
