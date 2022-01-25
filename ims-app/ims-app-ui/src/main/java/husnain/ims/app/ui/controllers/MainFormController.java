c2e5d5b610ace98f804a628b918bf56cef4097d6
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-25 12:55:07 +0300

    Update MainFormController.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/MainFormController.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/MainFormController.java
@@ -1,11 +1,17 @@
 package husnain.ims.app.ui.controllers;
 
+import husnain.ims.app.ui.InventoryManagementApp;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
@@ -17,6 +23,7 @@ import javafx.stage.Stage;
  */
 public class MainFormController {
 
+    private static final Logger LOG = Logger.getLogger(MainFormController.class.getName());
     @FXML
     private TableView<?> partsTable;
     @FXML
@@ -46,9 +53,22 @@ public class MainFormController {
     }
 
     @FXML
-    void exitApplication(ActionEvent event) {
-        var alert = new Alert(Alert.AlertType.CONFIRMATION, null, ButtonType.YES, ButtonType.NO);
+    void addProduct(ActionEvent event) {
+        try {
+            this.showProductDialog();
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
+    }
 
+    @FXML
+    void exitApplication(ActionEvent event) {
+        this.showExitDialog(event);
+    }
+
+    private void showExitDialog(ActionEvent event) {
+        var alert = new Alert(Alert.AlertType.CONFIRMATION, null, ButtonType.YES, ButtonType.NO);
+        
         alert.setTitle("Exit");
         alert.setHeaderText("Continue managing inventory?");
 
@@ -64,6 +84,23 @@ public class MainFormController {
         stage.close();
         //There's nothing to return
         return null;
+    }
+
+    private void showProductDialog() throws IOException {
+        var url = InventoryManagementApp.class.getResource("ProductForm.fxml");
+        var loader = new FXMLLoader(url);
+
+        DialogPane dlg = loader.load();
+
+        loader.setController(new ProductFormController());
+        
+        var alert = new Alert(Alert.AlertType.NONE);
+        var saveBtn = new ButtonType("Save", ButtonBar.ButtonData.YES);
+        
+        dlg.getButtonTypes().setAll(saveBtn, ButtonType.CANCEL);
+        alert.setDialogPane(dlg);
+        
+        alert.showAndWait();
     }
 
 }
