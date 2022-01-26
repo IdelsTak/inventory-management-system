334eacf08df482308f58369d74b4ae399f4aa3a4
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-26 23:19:04 +0300

    Update FormattedPriceCell.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/FormattedPriceCell.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/FormattedPriceCell.java
@@ -9,11 +9,11 @@ import javafx.scene.control.TableCell;
  * @author Husnain Arif
  */
 public class FormattedPriceCell extends TableCell<Part, Double> {
-    
+
     @Override
     protected void updateItem(Double price, boolean empty) {
         super.updateItem(price, empty);
         super.setText(Objects.isNull(price) || empty ? null : String.format("%.2f", price));
     }
-    
+
 }
