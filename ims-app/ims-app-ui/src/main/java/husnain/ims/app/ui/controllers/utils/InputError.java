6d560652edd61d3a9473f8c7cc71668b0666b899
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-26 21:22:27 +0300

    Create InputError.java

---
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/InputError.java
@@ -0,0 +1,18 @@
+package husnain.ims.app.ui.controllers.utils;
+
+/**
+ *
+ * @author Husnain Arif
+ */
+public record InputError(String title, String detail) {
+
+    public InputError(String title, String detail) {
+        this.title = title;
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", title, detail);
+    }
+}
