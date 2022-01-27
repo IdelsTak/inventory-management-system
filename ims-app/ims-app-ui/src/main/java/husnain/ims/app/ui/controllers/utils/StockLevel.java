411baa940e8113059bf4ba82cb57df1246ddaa57
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-27 13:32:31 +0300

    Create StockLevel.java

---
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/StockLevel.java
@@ -0,0 +1,27 @@
+package husnain.ims.app.ui.controllers.utils;
+
+/**
+ *
+ * @author Husnain Arif
+ */
+public class StockLevel {
+
+    private final int stock;
+    private final int min;
+    private final int max;
+
+    public StockLevel(int stock, int min, int max) {
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+
+    public boolean stockIsOutOfRange() {
+        return !(max > stock && stock > min);
+    }
+
+    public boolean minExceedsMaxStock() {
+        return min > max;
+    }
+
+}
