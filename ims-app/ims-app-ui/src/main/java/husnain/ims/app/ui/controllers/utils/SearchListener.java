e7ccd3a0cd0784c8bfaadd513c3f3951e75cc9ce
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-31 12:33:12 +0300

    Update SearchListener.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/SearchListener.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/SearchListener.java
@@ -4,6 +4,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputControl;
 
 /**
  *
@@ -12,10 +13,12 @@ import javafx.scene.control.TableView;
 public class SearchListener<T> implements ChangeListener<String> {
 
     private final SearchableList<T> searchableList;
+    private final TextInputControl searchControl;
     private final TableView<T> table;
 
-    public SearchListener(SearchableList<T> searchableList, TableView<T> table) {
+    public SearchListener(SearchableList<T> searchableList, TextInputControl searchControl, TableView<T> table) {
         this.searchableList = searchableList;
+        this.searchControl = searchControl;
         this.table = table;
     }
 
@@ -23,13 +26,16 @@ public class SearchListener<T> implements ChangeListener<String> {
     public void changed(ObservableValue<? extends String> ov, String oldText, String newText) {
         var filtered = searchableList.getFiltered(newText);
 
-        if (filtered.isEmpty()) {
+        if (filtered.isEmpty() && !new StringCheck(newText).isNullOrBlank()) {
             var alert = new Alert(Alert.AlertType.WARNING, null);
 
             alert.setHeaderText("No result found for query \"%s\"".formatted(newText));
             alert.show();
 
-            alert.setOnHidden(eh -> table.requestFocus());
+            alert.setOnHidden(eh -> {
+                searchControl.setText(null);
+                table.requestFocus();
+            });
         } else {
             table.setItems(filtered);
         }
