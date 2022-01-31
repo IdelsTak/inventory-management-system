fee73bc8609ce8db52e27983bff8d278f611a8b8
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-31 22:37:23 +0300

    Update SearchListener.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/SearchListener.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/SearchListener.java
@@ -2,9 +2,11 @@ package husnain.ims.app.ui.controllers.utils;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.scene.control.Alert;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextInputControl;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
@@ -14,31 +16,35 @@ public class SearchListener<T> implements ChangeListener<String> {
 
     private final SearchableList<T> searchableList;
     private final TextInputControl searchControl;
+    private final Labeled resultsLabel;
     private final TableView<T> table;
 
-    public SearchListener(SearchableList<T> searchableList, TextInputControl searchControl, TableView<T> table) {
+    public SearchListener(SearchableList<T> searchableList, TextInputControl searchControl, Labeled resultsLabel, TableView<T> table) {
         this.searchableList = searchableList;
         this.searchControl = searchControl;
+        this.resultsLabel = resultsLabel;
         this.table = table;
+        
+        searchControl.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+                searchControl.clear();
+    }
+        });
+        // Select all text when this editor is selected
+        searchControl.setOnMousePressed(event -> searchControl.selectAll());
+        searchControl.focusedProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue) {
+                searchControl.selectAll();
+            }
+        }));
     }
 
     @Override
     public void changed(ObservableValue<? extends String> ov, String oldText, String newText) {
         var filtered = searchableList.getFiltered(newText);
 
-        if (filtered.isEmpty() && !new StringCheck(newText).isNullOrBlank()) {
-            var alert = new Alert(Alert.AlertType.WARNING, null);
-
-            alert.setHeaderText("No result found for query \"%s\"".formatted(newText));
-            alert.show();
-
-            alert.setOnHidden(eh -> {
-                searchControl.setText(null);
-                table.requestFocus();
-            });
-        } else {
-            table.setItems(filtered);
-        }
+        resultsLabel.setText(filtered.isEmpty() ? "No result found for query \"%s\"".formatted(newText) : null);
+        table.setItems(filtered);
     }
 
 }
