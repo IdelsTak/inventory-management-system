4f95dd4f3d9e72d96f561a40f3b6367593e4b7cb
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-31 10:05:36 +0300

    Create Search.java

---
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/Search.java
@@ -0,0 +1,48 @@
+package husnain.ims.app.ui.controllers.utils;
+
+import husnain.ims.app.crud.Inventory;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Husnain Arif
+ */
+public class Search<T> implements Function<String, ObservableList<T>> {
+
+    private static final Logger LOG = Logger.getLogger(Search.class.getName());
+    private final Function<String, ObservableList<T>> nameSearch;
+    private final Function<Integer, T> idSearch;
+
+    public Search(Function<String, ObservableList<T>> nameSearch, Function<Integer, T> idSearch) {
+        this.nameSearch = nameSearch;
+        this.idSearch = idSearch;
+    }
+
+    @Override
+    public ObservableList<T> apply(String query) {
+        ObservableList<T> list;
+        if (new RegexCheck(query, "\\d+").doesntMatch()) {
+
+//            list = Inventory.lookupPart(query);
+            list = nameSearch.apply(query);
+        } else {
+            Optional<T> foundPart = Optional.empty();
+            try {
+//                foundPart = Optional.ofNullable(Inventory.lookupPart(Integer.parseInt(query)));
+                foundPart = Optional.ofNullable(idSearch.apply(Integer.parseInt(query)));
+            } catch (NumberFormatException | NoSuchElementException exc) {
+                LOG.log(Level.WARNING, "An expection occured: {0}", exc.getMessage());
+            }
+            list = foundPart.map(p -> FXCollections.observableArrayList(List.of(p))).orElse(FXCollections.emptyObservableList());
+        }
+        return list;
+    }
+
+}
