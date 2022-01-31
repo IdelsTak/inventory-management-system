49c67dd1bdc48b06689329db2532cd65628c5f75
Hiram Kamau <hiram.kamau@outlook.com>
2022-01-31 18:06:03 +0300

    Update module-info.java

--- ims-app/ims-app-ui/src/main/java/module-info.java
+++ ims-app/ims-app-ui/src/main/java/module-info.java
@@ -7,4 +7,10 @@ module ims.ui {
     requires javafx.graphics;
     requires javafx.controls;
     requires java.base;
+    //These exports are here so that the JavaDoc
+    //generation will be able to output the JavaDoc
+    //for the classes in these packages
+    exports husnain.ims.app.ui;
+    exports husnain.ims.app.ui.controllers;
+    exports husnain.ims.app.ui.controllers.utils;
 }
