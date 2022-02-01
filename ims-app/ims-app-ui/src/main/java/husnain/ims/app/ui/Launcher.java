3560b723d4bd65e52408d6aea574f3b8c549ebe7
Hiram Kamau <hiram.kamau@outlook.com>
2022-02-01 14:36:45 +0300

    Update Launcher.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/Launcher.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/Launcher.java
@@ -1,21 +1,32 @@
 package husnain.ims.app.ui;
 
-import husnain.ims.app.sample.data.Generate;
-
+//import husnain.ims.app.sample.data.Generate;
 /**
- * Starts the application.
+ * FUTURE ENHANCEMENT:
+ * <p>
+ * This class shouldn't be used if the JavaFX modules are declared correctly as
+ * VM options.
+ * <p>
+ * This class starts the application.
+ * <p>
+ * It also contains a commented out line that could be used to generate test
+ * data for the application.
  *
  * @author Husnain Arif
  */
 public class Launcher {
 
     /**
-     * 
-     * @param args 
+     * The applications main method.
+     *
+     * @param args command line arguments.
      */
     public static void main(String[] args) {
-        new Generate().sampleData();
-        
+        //Uncomment this line if you want to generate test data
+        /*
+         * new Generate().sampleData();
+         */
+
         InventoryManagementApp.main(args);
     }
 }
