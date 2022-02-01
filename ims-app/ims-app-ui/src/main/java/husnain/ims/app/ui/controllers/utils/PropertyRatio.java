d8570948e7013e62cd6fdcbf58787636001cd23e
Hiram Kamau <hiram.kamau@outlook.com>
2022-02-01 13:23:57 +0300

    Update PropertyRatio.java

--- ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/PropertyRatio.java
+++ ims-app/ims-app-ui/src/main/java/husnain/ims/app/ui/controllers/utils/PropertyRatio.java
@@ -1,14 +1,14 @@
 package husnain.ims.app.ui.controllers.utils;
 
-import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.Property;
 
 /**
  *
  * @author Husnain Arif
  */
-public record PropertyRatio(DoubleProperty property, Double ratio) {
+public record PropertyRatio<T extends Number>(Property<T> property, Double ratio) {
 
-    public PropertyRatio(DoubleProperty property, Double ratio) {
+    public PropertyRatio(Property<T> property, Double ratio) {
         this.property = property;
         this.ratio = ratio;
     }
